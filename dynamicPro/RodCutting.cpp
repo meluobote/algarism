94152e393e581f0ab63fe32c8c66fdf9736c7a18
meluobote <17621257318@163.com>
2019-12-23 17:44:13 +0800

    Merge branch 'master' of https://github.com/meluobote/algarism
     Conflicts:
    	cmake-build-debug/CMakeFiles/algarism.dir/CXX.includecache
    	cmake-build-debug/CMakeFiles/algarism.dir/depend.internal
    	cmake-build-debug/CMakeFiles/algarism.dir/main.cpp.obj

--- dynamicPro/RodCutting.cpp
+++ dynamicPro/RodCutting.cpp
@@ -6,17 +6,36 @@
 #include <iostream>
 using namespace std;
 
-void cutRod(vector<int>& values, vector<int>& stategy, vector<int>& prices, int& rod_len){
-    
+int cutRod(vector<int>& values, vector<int>& stategy, vector<int>& prices, int rod_len){
+    if(rod_len<=0){
+        return prices[0];
+    }else if(rod_len==1){
+        return prices[1];
+    }else{}
+
+    int allowed_max_rod_len = prices.size();
+
+    for(int i=1;i<rod_len;i++){
+        int max = -1;
+        for(int j=1;j<=i;j++){
+            if(max<prices[j] + values[rod_len-j]){
+                max = prices[j] + values[rod_len-j];
+                stategy[i]=prices[j];
+            }
+        }
+        values[i] = max;
+    }
 }
 
 int main(){
     vector<int> prices{0,1,5,8,9,10,17,17,20,24,30}; //共11个长度，包括长度0
     int rod_len=10;
 
-    vector<int> values;
-    vector<int> stategy;
+    vector<int> values(rod_len);
+    vector<int> stategy(rod_len);
 
 
     cout<<prices[11];
+
+    return 0;
 }
