e870d90d805185b54449be4ff04f8665fcee0a8d
meluobote <17621257318@163.com>
2020-01-17 17:31:51 +0800

    Merge branch 'master' of https://github.com/meluobote/algarism
     Conflicts:
    	cmake-build-debug/CMakeFiles/BestTaskDeploy.dir/build.make
    	cmake-build-debug/CMakeFiles/BestTaskDeploy.dir/depend.make
    	cmake-build-debug/CMakeFiles/BestTaskDeploy.dir/flags.make
    	cmake-build-debug/CMakeFiles/BestTaskDeploy.dir/link.txt
    	cmake-build-debug/CMakeFiles/algarism.dir/link.txt
    	cmake-build-debug/CMakeFiles/fibonacci.dir/build.make
    	cmake-build-debug/CMakeFiles/fibonacci.dir/flags.make
    	cmake-build-debug/CMakeFiles/fibonacci.dir/link.txt
    	cmake-build-debug/CMakeFiles/fibonacci.dir/progress.make

--- main.cpp
+++ main.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include "sort.h"
 #include <climits>
 #include <cmath>
-//#include "dynamicPro/CutRod.h"
+#include <map>
+#include <algorithm>
 using namespace std;
 
-int main() {
+vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+    vector<int> rev(1001,0);
+    vector<int>res(arr1.size());
+    vector<int> notInvec;
 
+    for(int i=0;i<arr1.size();i++){
+        if(find(arr2.begin(), arr2.end(), arr1[i])!=arr2.end()){
+            rev[arr1[i]]++;
+        }else{
+            notInvec.push_back(arr1[i]);
+        }
+    }
+    int idx=0;
+    for(int i=0;i<arr2.size();i++){
+        int n=rev[arr2[i]];
+        int j=0;
+        while(j<n){
+            res[idx++]=arr2[i];
+            j++;
+        }
+    }
+
+    sort(notInvec.begin(), notInvec.end());
+    int tmp=arr1.size()-1;
+    for(int i=notInvec.size()-1;i>=0;i--){
+        res[tmp]=notInvec[i];
+        tmp--;
+    }
+    return res;
+}
+
+int main() {
+    vector<int> arr1{2,3,1,3,2,4,6,7,9,2,19};
+    vector<int> arr2{2,1,4,3,9,6};
+    vector<int> res=relativeSortArray(arr1, arr2);
+    for(auto e:res){
+        cout<<e<<" ";
+    }
+    cout<<endl;
 }
