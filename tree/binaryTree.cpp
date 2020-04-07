00161f1790b1f00593d857fa05ac2f627b35a283
meluobote <17621257318@163.com>
2020-04-07 20:36:12 +0800

    Merge branch 'master' of https://github.com/meluobote/algarism
     Conflicts:
    	cmake-build-debug/CMakeFiles/BestTaskDeploy.dir/build.make
    	cmake-build-debug/CMakeFiles/BestTaskDeploy.dir/depend.make
    	cmake-build-debug/CMakeFiles/BestTaskDeploy.dir/flags.make
    	cmake-build-debug/CMakeFiles/BestTaskDeploy.dir/link.txt
    	cmake-build-debug/CMakeFiles/algarism.dir/link.txt
    	cmake-build-debug/CMakeFiles/fibonacci.dir/build.make
    	cmake-build-debug/CMakeFiles/fibonacci.dir/flags.make
    	cmake-build-debug/CMakeFiles/fibonacci.dir/link.txt
    	cmake-build-debug/CMakeFiles/fibonacci.dir/progress.make

--- tree/binaryTree.cpp
+++ tree/binaryTree.cpp
@@ -5,24 +5,80 @@
  */
 #include <iostream>
 #include <vector>
+#include <queue>
+
 using namespace std;
 /*
  * 简单的树结构定义（数据存储不是void*类型， 也不能自定义比较方式）
  */
-typedef struct Node{
+typedef struct Node {
     int data;
-    struct Tree* left;
-    struct Tree* right;
-}node_t;
-//给树添加元素
-void addNodde(node_t* root, int val){
+    struct Node *left;
+    struct Node *right;
+} node_t;
 
+/*
+ *1、vector是一个完全二叉堆
+ * 2、idx为head节点中数据对应在v中的索引
+ */
+void createTree(vector<int> &v, node_t *head, int idx) {
+    int size = v.size();
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left > size - 1) {
+        head->left = nullptr;
+        head->right = nullptr;
+        return;
+    } else if (left == size - 1) {
+        head->right = nullptr;
+        node_t *leftNode = new Node;
+        leftNode->data = v[left];
+        head->left = leftNode;
+        return;
+    } else {
+        node_t *leftNode = new Node;
+        leftNode->data = v[left];
+        head->left = leftNode;
+        node_t *rightNode = new Node;
+        rightNode->data = v[right];
+        head->right = rightNode;
+        createTree(v, leftNode, left);
+        createTree(v, rightNode, right);
+    }
 }
-//用vector创建一颗树, 说明vector是一个完全二叉堆
-void createTree(vector<int> v){
 
+void deleteTree(node_t *root) {
+    if (root == nullptr)
+        return;
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
+}
+/*
+ * 树按层遍历： 打印为一行
+ */
+void traverseByLayer(node_t *root) {
+    queue<node_t *> qn;
+    qn.push(root);
+    while (!qn.empty()) {
+        node_t *tmp = qn.front();
+        qn.pop();
+        cout << tmp->data << " ";
+        if (tmp->left != nullptr) {
+            qn.push(tmp->left);
+        }
+        if (tmp->right != nullptr) {
+            qn.push(tmp->right);
+        }
+    }
 }
 
-int main(){
-    vector<int>
+int main() {
+    vector<int> v{9, 8, 7, 6, 5, 4, 3, 1, 2};
+    node_t *root = new Node;
+    root->data = v[0];
+    createTree(v, root, 0);
+    traverseByLayer(root);
+    deleteTree(root);
 }
